aa12c31feccce20bb1279455dba404285531a455
David Bock <dbock@Big-Trak.local>
2009-08-27 11:21:18 -0400

    this simple invokation provides some coverage on the declarations in the class

--- test/test_workowner.rb
+++ test/test_workowner.rb
@@ -8,6 +8,10 @@ class TestWorkowner < Test::Unit::TestCase
   should "include the WorkOwner module when declared as a stonepath_workowner" do
     assert Workowner.included_modules.include?(StonePath::WorkOwner)
   end
+  
+  should "create a new user successfully" do
+    u = User.create
+  end
 end
 
 

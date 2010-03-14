c056640d52b34079b18567f210c068b1423e462f
bokmann <dbock@codesherpas.com>
2010-03-14 15:34:59 -0400

    tests in dcps app caught this problem after upgrade to sentient_user.  I need to extract tests to run on our mock rails app.

--- lib/stonepath/event_logging.rb
+++ lib/stonepath/event_logging.rb
@@ -5,11 +5,10 @@ module StonePath
       has_many :logged_events, :as => :auditable, :class_name => "EventRecord", :order => "created_at"
 
       define_method :aasm_event_fired do |event_name, old_state_name, new_state_name|
-        current_user_id = User.current
         self.logged_events.create(:event_name => event_name.to_s,
                                   :old_state_name => old_state_name.to_s,
                                   :new_state_name => new_state_name.to_s,
-                                  :user_id => current_user_id)
+                                  :user => User.current)
       end
     end
   end

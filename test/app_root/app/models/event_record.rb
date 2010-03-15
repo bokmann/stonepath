aac7e00df46afe2ce0a0be26936110f8fd2f65ae
bokmann <dbock@codesherpas.com>
2010-03-14 22:41:55 -0400

    restoring the event logging test

--- test/app_root/app/models/event_record.rb
+++ test/app_root/app/models/event_record.rb
@@ -1,6 +1,6 @@
 class EventRecord < ActiveRecord::Base
   belongs_to :auditable, :polymorphic => true  
-  
+  belongs_to :user
   # the user_id field will point to the id of the current_user set by the controller
   # hack.  feel free to add our own finder here using that.
 end
